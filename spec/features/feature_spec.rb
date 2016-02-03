dd79af3e224ed0d194de993400927b0cda649a8c
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-03 14:19:21 +0000

    User Story 1 - Implemented user names and display them

---
+++ spec/features/feature_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+
+feature 'Users enter their names' do
+
+before do
+    include Capybara::DSL
+    Capybara.default_driver = :selenium
+ end
+
+
+  scenario 'both players fill in forms and submit' do
+    save_and_open_page
+    visit '/'
+    fill_in 'player1', with: 'Bob'
+    fill_in 'player2', with: 'John'
+    click_button('Submit')
+    expect(page).to have_content('Bob vs. John')
+  end
+
+  scenario 'both names returned on screen' do
+  end
+  
+end
