e392b5944739c4b633fa352f3d0e61c4d980082a
valogopedina <valentina.monetta@tiscali.it>
2016-02-04 12:59:37 +0000

    Added feature test to view player2 hit points

--- app.rb
+++ app.rb
@@ -21,9 +21,6 @@ class Battle < Sinatra::Base
   end
 
 
-
-
-
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,12 @@
 require 'capybara/dsl'
 require 'selenium-webdriver'
 
+def browser_view
+  include Capybara::DSL
+  Capybara.default_driver = :selenium
+end
 
 feature 'Enter names' do
-  before do
-    include Capybara::DSL
-    Capybara.default_driver = :selenium
-  end
 
   scenario 'submitting names' do
     visit('/')
@@ -14,5 +14,17 @@ feature 'Enter names' do
     fill_in :player_2_name, with: 'Valentina'
     click_button 'Submit'
     expect(page).to have_content 'Rebecca vs. Valentina'
+  end
+end
+
+feature 'View player 2 Hit Points' do
+
+  scenario 'see player 2 hit points' do
+    browser_view
+    visit('/')
+    fill_in :player_1_name, with: 'Rebecca'
+    fill_in :player_2_name, with: 'Valentina'
+    click_button 'Submit'
+    expect(page).to have_content 'Valentina: 100HP'
   end
 end
